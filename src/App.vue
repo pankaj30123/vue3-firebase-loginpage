e383658f4f61a7fbd61d33ab9cb7ea6c0b6f477c
Fuelbuddy <student201923topper@gmail.com>
2023-09-05 17:53:17 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,37 @@
 <template>
-  <div id="nav" v-if="$store.state.user">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <button @click="$store.dispatch('logout')">Logout</button>
+  <div id="app">
+    <div id="nav" v-if="$store.state.user">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <button @click="$store.dispatch('logout')">Logout</button>
+    </div>
+    <router-view />
+    <div v-if="user" class="user-details">
+      <p class="user-name">{{ user.name }}</p>
+      <p class="user-email">{{ user.email }}</p>
+    </div>
   </div>
-  <router-view/>
 </template>
 
 <script>
-import { onBeforeMount } from 'vue'
-import { useStore } from 'vuex'
+import { onBeforeMount, computed } from "vue";   
+import { useStore } from "vuex";
 
 export default {
   setup() {
-    const store = useStore()
+    const store = useStore();   
 
-    onBeforeMount(() => {
-      store.dispatch('fetchUser')
-    })
-  }
-}
+    const user = computed(() => store.state.user);  
+
+    onBeforeMount(async () => {
+      await store.dispatch("fetchUser");
+    });
+
+    return {
+      user,
+    };
+  },
+};
 </script>
 
 <style>
@@ -32,12 +44,12 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  text-align: center;  
+  color: #2c3e50;     
 }
 
 #nav {
-  padding: 30px;
+  padding: 30px; 
 }
 
 #nav a {
@@ -46,6 +58,36 @@ export default {
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #eb34d5;
 }
-</style>
+
+#app {
+  position: relative; 
+
+}
+
+.user-details {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background-color: #f0f0f0;
+  padding: 10px;
+  border-radius: 5px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);   
+  z-index: 1000;
+}
+
+.user-name {
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+
+.user-email {
+  font-size: 1rem;
+  color: #333;
+}
+</style>        
+
+
+
+
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,145 +1,151 @@
 <template>
-  <main class="login">
-    <section class="forms">
-      <form class="register" @submit.prevent="register">
-        <h2>Register</h2>
-        <input
-          type="email"
-          placeholder="Email address"
-          v-model="register_form.email"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          v-model="register_form.password"
-        />
-        <input type="submit" value="Register" />
-      </form>
+	<main class="login">
+		<section class="forms">
 
-      <form class="login" @submit.prevent="login">
-        <h2>Login</h2>
-        <input
-          type="email"
-          placeholder="Email address"
-          v-model="login_form.email"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          v-model="login_form.password"
-        />
-        <input type="submit" value="Login" />
-      </form>
-    </section>
-  </main>
+			<form class="register" @submit.prevent="register">
+				<h2>Register</h2>
+				<input 
+					type="email" 
+					placeholder="Email address"
+					v-model="register_form.email" />
+				<input 
+					type="password" 
+					placeholder="Password" 
+					v-model="register_form.password" />  
+				<input 
+					type="submit" 
+					value="Register" />
+			</form>
+
+			<form class="login" @submit.prevent="login">
+				<h2>Login</h2>
+				<input 
+					type="email" 
+					placeholder="Email address"
+					v-model="login_form.email" />
+				<input 
+					type="password" 
+					placeholder="Password" 
+					v-model="login_form.password" />
+				<input 
+					type="submit" 
+					value="Login" />
+			</form>
+
+		</section>
+	</main>
 </template>
 
 <script>
-import { ref } from "vue";
-import { useStore } from "vuex";
+import { ref } from 'vue'
+import { useStore } from 'vuex'
 
 export default {
-  setup() {
-    const login_form = ref({});
-    const register_form = ref({});
-    const store = useStore();
+	setup () {
+		const login_form = ref({});
+		const register_form = ref({});
+		const store = useStore();
 
-    const login = () => {
-      store.dispatch("login", login_form.value);
-    };
+		const login = () => {
+			store.dispatch('login', login_form.value);
+		}
 
-    const register = () => {
-      store.dispatch("register", register_form.value);
-    };
+		const register = () => {
+			store.dispatch('register', register_form.value);
+		}
 
-    return {
-      login_form,
-      register_form,
-      login,
-      register,
-    };
-  },
-};
+		return {
+			login_form,
+			register_form,
+			login,
+			register
+		}
+	}
+}
 </script>
 
 <style>
 .forms {
-  display: flex;
-  min-height: 100vh;
+	display: flex;
+	min-height: 100vh;
 }
 
 form {
-  flex: 1 1 0%;
-  padding: 8rem 1rem 1rem;
+	flex: 1 1 0%;
+	padding: 8rem 1rem 1rem;
 }
 
 form.register {
-  color: #fff;
-  background-color: #32a8a0;
-  background-image: linear-gradient(to bottom right, #32a86f, #32a86f);
+	color: #FFF;
+	background-color: rgb(245, 66, 101);
+	background-image: linear-gradient(
+		to bottom right,
+		rgb(245, 66, 101) 0%,
+		rgb(189, 28, 60) 100%
+	);
 }
 
 h2 {
-  font-size: 2rem;
-  text-transform: uppercase;
-  margin-bottom: 2rem;
+	font-size: 2rem;
+	text-transform: uppercase;
+	margin-bottom: 2rem;
 }
 
 input {
-  appearance: none;
-  border: none;
-  outline: none;
-  background: none;
+	appearance: none;
+	border: none;
+	outline: none;
+	background: none;
 
-  display: block;
-  width: 100%;
-  max-width: 400px;
-  margin: 0 auto;
-  font-size: 1.5rem;
-  margin-bottom: 2rem;
-  padding: 0.5rem 0rem;
+	display: block;
+	width: 100%;
+	max-width: 400px;
+	margin: 0 auto;
+	font-size: 1.5rem;
+	margin-bottom: 2rem;
+	padding: 0.5rem 0rem;
 }
 
 input:not([type="submit"]) {
-  opacity: 0.8;
-  transition: 0.4s;
+	opacity: 0.8;
+	transition: 0.4s;
 }
 
 input:focus:not([type="submit"]) {
-  opacity: 1;
+	opacity: 1;
 }
 
 input::placeholder {
-  color: inherit;
+	color: inherit;
 }
 
 form.register input:not([type="submit"]) {
-  color: #fff;
-  border-bottom: 2px solid #fff;
+	color: #FFF;
+	border-bottom: 2px solid #FFF;
 }
 
 form.login input:not([type="submit"]) {
-  color: #2c3e50;
-  border-bottom: 2px solid #2c3e50;
+	color: #2c3e50;
+	border-bottom: 2px solid #2c3e50;
 }
 
 form.login input[type="submit"] {
-  background-color: #a85632;
-  color: #fff;
-  font-weight: 700;
-  padding: 1rem 2rem;
-  border-radius: 0.5rem;
-  cursor: pointer;
-  text-transform: uppercase;
+	background-color: rgb(245, 66, 101);
+	color: #FFF;
+	font-weight: 700;
+	padding: 1rem 2rem;
+	border-radius: 0.5rem;
+	cursor: pointer;
+	text-transform: uppercase;
 }
 
 form.register input[type="submit"] {
-  background-color: #fff;
-  color: #a85632;
-  font-weight: 700;
-  padding: 1rem 2rem;
-  border-radius: 0.5rem;
-  cursor: pointer;
-  text-transform: uppercase;
+	background-color: #FFF;
+	color: rgb(245, 66, 101);
+	font-weight: 700;
+	padding: 1rem 2rem;
+	border-radius: 0.5rem;
+	cursor: pointer;
+	text-transform: uppercase;     
 }
 </style>
